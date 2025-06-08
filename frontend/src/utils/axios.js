da60f3570350c0d495dcb012e1ccaee178d84662
Sharathguru <vsarath200219@gmail.com>
2025-06-08 23:59:37 +0530

    Backend Deployed

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosInstance=axios.create({
-    baseURL: "http://localhost:5000/api/v1",
+    baseURL: "https://wayfarer-rj79.onrender.com",
     headers: {
         "Content-Type": "application/json",
     }
