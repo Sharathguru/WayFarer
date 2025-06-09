2699c26f861ed3784f105ed2ce6db02941826e74
Sharathguru <vsarath200219@gmail.com>
2025-06-09 12:52:55 +0530

    implement the backend to the Frontend

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosInstance=axios.create({
-    baseURL: "https://wayfarer-rj79.onrender.com",
+    baseURL: "https://wayfarer-rj79.onrender.com/api/v1",    
     headers: {
         "Content-Type": "application/json",
     }
